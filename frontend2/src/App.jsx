55523fe834b1e0bc6e33b5059523de08b470e674
NamHyeongKeol <grey_pastor@naver.com>
2022-05-11 01:23:43 +0900

    add frontend2

---
+++ frontend2/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import Connect from './components/connect';
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div>
+        <Navigation />
+        <Header data={landingPageData.Header} />
+        <Features data={landingPageData.Features} />
+        <About data={landingPageData.About} />
+        <Services data={landingPageData.Services} />
+        <Gallery data={landingPageData.Gallery} />
+        <Testimonials data={landingPageData.Testimonials} />
+        <Team data={landingPageData.Team} />
+        <Contact data={landingPageData.Contact} />
+
+        <Router>
+          <Route path="/wallet/connect">
+            <Connect />
+          </Route>
+        </Router>
+      </div>
+    </Web3ReactProvider>
+  );
+};
+
+export default App;
---
+++ frontend2/src/components/navigation.jsx
@@ -0,0 +1,140 @@
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from "@web3-react/injected-connector";
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from "@web3-react/walletconnect-connector";
+
+import { EuiFlexGroup, EuiFlexItem, EuiButton, EuiDescriptionList, EuiPanel } from "@elastic/eui";
+import { useEffect, useState } from 'react';
+
+const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
+
+function getErrorMessage(error: Error) {
+  if (error instanceof NoEthereumProviderError) {
+    return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network.";
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected ||
+    error instanceof UserRejectedRequestErrorWalletConnect
+  ) {
+    return "Please authorize this website to access your Ethereum account.";
+  } else {
+    return "An unknown error occurred. Check the console for more details.";
+  }
+}
+
+export const Navigation = (props) => {
+  const context = useWeb3React();
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context;
+  const [balance, setBalance] = useState("0");
+
+  useEffect(() => {
+    if (!!account && !!library) {
+      library.getBalance(account).then(value => {
+        console.log(value);
+        setBalance(value.toString());
+      })
+    }
+  }, [account, library, chainId, active])
+
+  const connectWallet = () => {
+    activate(injected);
+  }
+
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll' href='#page-top'>
+            React Landing Page
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll'>
+                Features
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll'>
+                Team
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Contact
+              </a>
+            </li>
+            <EuiFlexGroup direction={'column'} style={{ width: '50%' }}>
+              <EuiFlexItem>
+                <EuiButton onClick={() => connectWallet()}>지갑 연결하기</EuiButton>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiPanel hasShadow={false} hasBorder={true}>
+                  <EuiDescriptionList listItems={[
+                    { title: '지갑 주소', description: active ? "당신의 지갑 주소는 : " + account + " 입니다." : "지갑을 연결해주세요." },
+                    { title: "잔액", description: `${balance} ETH` }
+                  ]} />
+                </EuiPanel>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <div>
+              {(active || error) && (
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    deactivate();
+                  }}
+                >
+                  Deactivate
+                </button>
+              )}
+
+              {!!error && <h4 style={{ marginTop: "1rem", marginBottom: "0" }}>{getErrorMessage(error)}</h4>}
+            </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
