bab67d2f0c20c16517222a020f55d76768263255
jihyune <rlawlcns96@gmail.com>
2022-05-31 21:16:14 +0900

    05.31

--- src/components/proposalElement/proposalElement.tsx
+++ src/components/proposalElement/proposalElement.tsx
@@ -1,19 +1,25 @@
 import { ProposalForm } from '../../dto/common.dto'
 import * as S from './styles'
+import { EuiCard, EuiIcon, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
 interface Props extends ProposalForm {
 
 }
+const icons = ['Beats', 'Cloud', 'Logging', 'Kibana'];
 
 const ProposalElement: React.FC<Props> =
     ({ description, content_url, title, user_key }) => {
         return (
-            <S.Container>
-                <S.Title>제목 : {title}</S.Title>
-                <S.Key>{user_key}</S.Key>
-                <S.Description>내용 : {description}</S.Description>
-                <S.Url>이미지url : {content_url}</S.Url>
-            </S.Container>
+            <EuiFlexItem>
+                <EuiCard
+                    layout="horizontal"
+                    title={`제목 : ${title}`}
+                    description={
+                        `key : ${user_key}` + `\n`
+                        + `내용 : ${description}`
+                    }
+                />
+            </EuiFlexItem>
         )
     }
 
